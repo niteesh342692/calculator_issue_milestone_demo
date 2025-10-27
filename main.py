73420f70c06cea3f0cd753c574ae56c0e229e989
niteesh342692 <91664298+niteesh342692@users.noreply.github.com>
2025-10-27 12:39:02 +0530

    Implement error handling in divi and mod functions. Fixes #1
    Added error handling for division and modulus functions.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def add(a, b): return a + b
 def sub(a, b): return a - b
 def multi(a, b): return a * b
-def divi(a, b): return a / b
-def mod(a, b): return a % b
+def divi(a, b):
+  if(b == 0): return 'Divisor is Zero, unable to divide'
+  return a / b
+def mod(a, b):
+  if(b == 0): return 'Divisor is Zero, unable to divide'
+  return a % b
 
 a, b = int(input()), int(input())
 print(add(a, b))
