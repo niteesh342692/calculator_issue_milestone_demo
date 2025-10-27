bc712dbae04aa6cb9d18d97fdeccf9114697d78a
niteesh342692 <91664298+niteesh342692@users.noreply.github.com>
2025-10-27 12:25:47 +0530

    Implement basic arithmetic operations in main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def multi(a, b): return a * b
+def divi(a, b): return a / b
+def mod(a, b): return a % b
+
+a, b = int(input()), int(input())
+print(add(a, b))
+print(sub(a, b))
+print(multi(a, b))
+print(divi(a, b))
+print(mod(a, b))
